b76f5f427052f0b99966c8db3ee50240892eca4c
Theo Cooper-Brown <trcb.github@gmail.com>
2024-03-17 02:04:13 +0000

    style: fix destination-moodboard layout

--- app/destination-moodboard/layout.tsx
+++ app/destination-moodboard/layout.tsx
@@ -3,11 +3,13 @@ import Navbar from '@/app/ui/navbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col w-full">
-      <div className="flex flex-col w-full pl-5 pt-5">
-        <CogoLogo />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex flex-col pt-5 px-5">
+        <div className="mb-3">
+          <CogoLogo />
+        </div>
+        {children}
       </div>
-      <div className="flex-grow p-6">{children}</div>
       <Navbar />
     </div>
   );
--- app/destination-moodboard/page.tsx
+++ app/destination-moodboard/page.tsx
@@ -3,15 +3,10 @@ import PlusButton from '@/app/ui/plus-button';
 
 export default async function Page() {
   return (
-    <div className="flex flex-col gap-4">
-      <div>
-        <h1 className="text-3xl">MOBCODERS TRIP</h1>
-      </div>
-
+    <>
+      <h1 className="text-3xl">MOBCODERS TRIP</h1>
       <DestinationCard />
-      <div className="self-center">
-        <PlusButton />
-      </div>
-    </div>
+      <PlusButton />
+    </>
   );
 }
