c4d1dfa45c88c90056b4ff4eabe1ad3f8de38f2b
Arjun Gill <arjungill6ep@gmail.com>
2024-03-18 19:30:16 +0000

    feat: add form fields, no function

--- app/ui/add-destination.tsx
+++ app/ui/add-destination.tsx
@@ -7,15 +7,30 @@ import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import { Trip } from '@prisma/client/wasm';
 import { createPotentialDestination, updateTripNameDate } from '@/lib/action';
 
-export default function AddDestination({ trip }: { trip: Trip }) {
-  async function handleAdd(formData: FormData) {
-    createPotentialDestination(trip.id, formData);
+export default function AddDestination({ tripId }: { tripId: string }) {
+  const [openForm, setOpenForm] = useState(false);
+  function handleAdd(formData: FormData) {
+    createPotentialDestination(tripId, formData);
   }
 
   return (
-    <form action={handleAdd}>
-      <Input name="city" />
-      <Button>Add</Button>
-    </form>
+    <>
+      {!openForm && (
+        <Button type="button" onClick={() => setOpenForm(!openForm)}>
+          Add a destination
+        </Button>
+      )}
+      {openForm && (
+        <form action={handleAdd} className="flex flex-col gap-5">
+          <Input name="city" id="city" placeholder="City" />
+          <Input name="country" id="country" placeholder="Country" />
+          <Input name="photoUrl" id="photoUrl" placeholder="Image URL" />
+          <div className="flex justify-end gap-5">
+            <Button onClick={() => setOpenForm(!openForm)}>Cancel</Button>
+            <Button type="submit">Add</Button>
+          </div>
+        </form>
+      )}
+    </>
   );
 }
