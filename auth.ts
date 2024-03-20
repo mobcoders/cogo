6ec8c4b98902e4ba8aa7e252e787283b8c7e3a91
Arjun Gill <arjungill6ep@gmail.com>
2024-03-20 18:41:40 +0000

    Google auth working and password authentication

--- auth.ts
+++ auth.ts
@@ -1,15 +1,25 @@
 import { authConfig } from '@/auth.config';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import GitHub from 'next-auth/providers/github';
 import google from 'next-auth/providers/google';
 import prisma from '@/lib/prisma';
-// const bcrypt = require('bcrypt');
+import bcrypt from 'bcryptjs';
 
 export async function getUser(email: string) {
   const user = await prisma.user.findUnique({
     where: {
       email: email,
+    },
+  });
+  return user;
+}
+
+export async function createOAuthUser(id: string, email: string, name: string) {
+  const user = await prisma.user.create({
+    data: {
+      id: id,
+      email: email,
+      name: name,
     },
   });
   return user;
@@ -25,28 +35,18 @@ export const {
   providers: [
     Credentials({
       id: 'credentials',
-      // name: 'Sign in',
-      // credentials: {
-      //   email: {
-      //     label: 'Email',
-      //     type: 'email',
-      //     placeholder: 'hello@example.com',
-      //   },
-      //   password: { label: 'Password', type: 'password' },
-      // },
       async authorize(credentials) {
-        //hardcoded user for development purposes (exists on db too):
-        // const email = 'anna@anna.com';
         const { email, password } = credentials;
         const user = await getUser(email as string);
         if (!user) return null;
-        return user;
-        // const passwordsMatch = await bcrypt.compare(password, user.password!);
-        //   if (passwordsMatch) return user;
-        //   return null;
+        const passwordsMatch = await bcrypt.compare(
+          password as string,
+          user!.password!
+        );
+        if (passwordsMatch) return user;
+        return null;
       },
     }),
-    GitHub,
     google,
   ],
 });
