8816e245419504f32ee28d003229db36ce280eeb
Theo Cooper-Brown <trcb.github@gmail.com>
2024-03-23 20:31:42 +0000

    fix: should work now

--- app/ui/profile-settings.tsx
+++ app/ui/profile-settings.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Button } from '@nextui-org/button';
 import {
   Dropdown,
@@ -12,10 +11,10 @@ import {
   CameraIcon,
   TrashIcon,
 } from '@heroicons/react/24/solid';
-import { deleteGroupTrip, deleteUser } from '@/lib/action';
-import EditUserPhoto from '@/app/ui/edit-user-photo';
-import { useState } from 'react';
+import { deleteUser } from '@/lib/action';
 import { CldUploadWidget } from 'next-cloudinary';
+import { updateUserPhoto } from '@/lib/action';
+import { useRef } from 'react';
 
 export default function ProfileSettings({
   userId,
@@ -24,74 +23,76 @@ export default function ProfileSettings({
   userId: string;
   userEmail: string;
 }) {
-  const [showCloud, setShowCloud] = useState(false);
+  function handleSuccess(url: string) {
+    updateUserPhoto(userId, url);
+  }
+
   return (
     <div>
-      {showCloud ? (
-        <CldUploadWidget
-          uploadPreset="cogo_cloudinary"
-          options={{ sources: ['local', 'url'] }}
-          onSuccess={(result, { widget }) => {
-            if (typeof result.info === 'object') {
-              handleSuccess(result!.info!.secure_url);
-            }
-          }}
-        >
-          {({ open }) => {
-            function handleOnClick() {
-              open();
-            }
-            return (
-              <Button
-                className="bg-pink-500 text-white w-40"
-                startContent={<CameraIcon height={15} className="fill-white" />}
-                onClick={handleOnClick}
+      <CldUploadWidget
+        uploadPreset="cogo_cloudinary"
+        options={{ sources: ['local', 'url'] }}
+        onSuccess={(result, { widget }) => {
+          if (typeof result.info === 'object') {
+            handleSuccess(result!.info!.secure_url);
+          }
+        }}
+      >
+        {({ open }) => {
+          function handleOnClick() {
+            open();
+          }
+          return (
+            <Dropdown>
+              <DropdownTrigger className="w-fit">
+                <Button
+                  isIconOnly
+                  size="sm"
+                  className="bg-transparent"
+                  variant="solid"
+                  disableRipple
+                >
+                  <SettingsIcon height={22} className="fill-light-grey" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Group Trip Actions"
+                className="w-fit"
+                closeOnSelect={false}
               >
-                Update Photo
-              </Button>
-            );
-          }}
-        </CldUploadWidget>
-      ) : (
-        <></>
-      )}
-      <Dropdown>
-        <DropdownTrigger className="w-fit">
-          <Button
-            isIconOnly
-            size="sm"
-            className="bg-transparent"
-            variant="solid"
-            disableRipple
-          >
-            <SettingsIcon height={22} className="fill-light-grey" />
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu
-          aria-label="Group Trip Actions"
-          // onAction={(key) => handleClick(key as string)}
-          className="w-fit"
-        >
-          <DropdownItem
-            key="update-photo"
-            textValue="Update photo"
-            className="w-fit"
-          >
-            <Button onClick={() => setShowCloud(true)}>Update Photo</Button>
-          </DropdownItem>
-          <DropdownItem key="delete" textValue="Delete" className="w-fit">
-            <Button
-              className="bg-pink-500 text-white w-40"
-              startContent={<TrashIcon height={15} className="fill-white" />}
-              onClick={async () => {
-                await deleteUser(userEmail);
-              }}
-            >
-              Delete Account
-            </Button>
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+                <DropdownItem
+                  key="update-photo"
+                  textValue="Update photo"
+                  className="w-fit"
+                >
+                  <Button
+                    className="bg-pink-500 text-white w-40"
+                    startContent={
+                      <CameraIcon height={15} className="fill-white" />
+                    }
+                    onClick={handleOnClick}
+                  >
+                    Update Photo
+                  </Button>
+                </DropdownItem>
+                <DropdownItem key="delete" textValue="Delete" className="w-fit">
+                  <Button
+                    className="bg-pink-500 text-white w-40"
+                    startContent={
+                      <TrashIcon height={15} className="fill-white" />
+                    }
+                    onClick={async () => {
+                      await deleteUser(userEmail);
+                    }}
+                  >
+                    Delete Account
+                  </Button>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          );
+        }}
+      </CldUploadWidget>
     </div>
   );
 }
