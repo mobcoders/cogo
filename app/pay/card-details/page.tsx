008af6cc61a4f649e26e595ff5448d7daf7f5fe2
Arjun Gill <arjungill6ep@gmail.com>
2024-03-26 12:45:53 +0000

    feat: show card details within ui component

--- app/pay/card-details/page.tsx
+++ app/pay/card-details/page.tsx
@@ -1,15 +1,18 @@
-import { Button, Card, CardBody, CardHeader, Divider } from '@nextui-org/react';
-import { LockClosedIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import { FaRegSnowflake } from 'react-icons/fa';
-import Image from 'next/image';
+import { Card, CardBody } from '@nextui-org/react';
 
-export default function Page() {
+import Image from 'next/image';
+import ShowCard from '@/app/pay/card-details/show-card';
+import { getCardDetails, loginUser } from '@/lib/weavr';
+
+export default async function Page() {
+  const user = await loginUser();
+  user.cardDetails = await getCardDetails(user.token);
+
   return (
     <>
       <h1>Trip Account</h1>
-      <Card className="h-[200px] my-5 bg-purple-700">
+      <Card className="h-[200px] max-w-[400px] my-5 bg-purple-700">
         <CardBody className="p-5">
-          {/* <h3>4556202687314205</h3> */}
           <div className="flex justify-between h-full">
             <div className="flex flex-row items-center h-full ml-5">
               <Image
@@ -40,59 +43,7 @@ export default function Page() {
         </CardBody>
       </Card>
 
-      <div className="flex gap-5 justify-between">
-        <Button className="w-full h-[70px] bg-pink-500">
-          <div className="flex flex-col items-center">
-            <LockClosedIcon height={17} className="fill-white" />
-            <p className="text-white">Reveal pin</p>
-          </div>
-        </Button>
-        <Button className="w-full h-[70px] bg-pink-500">
-          <div className="flex flex-col items-center">
-            <FaRegSnowflake size={17} className="fill-white" />
-            <p className="text-white">Freeze card</p>
-          </div>
-        </Button>
-      </div>
-
-      <Card className="max-w-[400px] my-5">
-        <CardHeader className="pb-0">
-          <div className="flex justify-between w-full font-bold">
-            <p>Card Details</p>
-            <p>Reveal</p>
-          </div>
-        </CardHeader>
-        <CardBody className="pt-2">
-          <div className="flex w-full justify-between my-3">
-            <p>Name on card</p>
-            <p>Theodore Cooper-Brown</p>
-          </div>
-          <Divider />
-
-          <div className="flex w-full justify-between my-3">
-            <p>Card Number</p>
-            <p>**** **** **** 4205</p>
-          </div>
-          <Divider />
-
-          <div className="flex w-full justify-between my-3">
-            <p>Expiry</p>
-            <p>04/29</p>
-          </div>
-          <Divider />
-
-          <div className="flex w-full justify-between my-3">
-            <p>CVC</p>
-            <p>***</p>
-          </div>
-          <Divider />
-
-          <div className="flex w-full justify-between my-3">
-            <p>Billing Address</p>
-            <ChevronRightIcon height={17} className="my-auto" />
-          </div>
-        </CardBody>
-      </Card>
+      <ShowCard user={user} />
     </>
   );
 }
