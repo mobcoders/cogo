9e15cb2741181a191f48891e7440c4364ed112ea
Arjun Gill <arjungill6ep@gmail.com>
2024-03-26 19:12:44 +0000

    fix: need to refactor creating trip if not logged in

--- app/ui/create-trip-button.tsx
+++ app/ui/create-trip-button.tsx
@@ -2,20 +2,23 @@
 
 import { createTrip } from '@/lib/action';
 import { Button, Spinner } from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 
-export default function CreateTripButton({
-  tripName,
-  id,
-}: {
-  tripName: string;
-  id: string;
-}) {
+export default function CreateTripButton() {
+  let session = useSession();
+  if (session.status !== 'authenticated') {
+  }
+  console.log(session);
+  // let session = await auth();
+  // let name = session!.user!.name!;
+  // let id = session?.user?.id!;
+  // let tripName = name.split(' ')[0] + "'s new group trip";
   const [loading, setLoading] = useState(false);
 
   function handleClick() {
     setLoading(true);
-    createTrip(tripName, id);
+    // createTrip(tripName, id);
   }
 
   return (
