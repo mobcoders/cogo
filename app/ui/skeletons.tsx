2ed05c7e950ab2706d14a83bec38316698ef0630
Anna Ewbank <Ewbank145@gmail.com>
2024-03-27 19:46:25 +0000

    feat: dark mode skeletons

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -5,12 +5,14 @@ const shimmer =
 export function PotentialDestSkeleton() {
   return (
     <div className="flex flex-col gap-5 md:w-[640px] md:mx-auto">
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
       <div
-        className={`${shimmer} h-14 w-full bg-gray-100 rounded-lg md:h-12`}
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
       ></div>
       <div
-        className={`${shimmer} h-12 w-full bg-gray-100 rounded-lg mb-5 md:w-72 md:mx-auto`}
+        className={`${shimmer} h-14 w-full bg-gray-100 rounded-lg md:h-12 dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-12 w-full bg-gray-100 rounded-lg mb-5 md:w-72 md:mx-auto dark:bg-primary-500`}
       ></div>
     </div>
   );
@@ -19,13 +21,17 @@ export function PotentialDestSkeleton() {
 export function PotentialAccomSkeleton() {
   return (
     <div className="flex flex-col gap-5 md:w-[640px] md:mx-auto">
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
       <div
-        className={`${shimmer} h-14 w-full bg-gray-100 rounded-lg md:h-12`}
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
       ></div>
-      <div className={`${shimmer} h-12 w-full bg-gray-100 rounded-lg`}></div>
       <div
-        className={`${shimmer} h-12 w-20 ml-auto bg-gray-100 rounded-lg`}
+        className={`${shimmer} h-14 w-full bg-gray-100 rounded-lg md:h-12 dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-12 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-12 w-20 ml-auto bg-gray-100 rounded-lg dark:bg-primary-500`}
       ></div>
     </div>
   );
@@ -34,13 +40,27 @@ export function PotentialAccomSkeleton() {
 export function TripSummarySkeleton() {
   return (
     <div className="flex flex-col gap-5 md:w-[640px] md:mx-auto">
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg`}></div>
-      <div className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg`}></div>
+      <div
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-7 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
+      <div
+        className={`${shimmer} h-24 w-full bg-gray-100 rounded-lg dark:bg-primary-500`}
+      ></div>
     </div>
   );
 }
