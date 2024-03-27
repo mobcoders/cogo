5edbbb4a972409845642d6d2d773983c7d81fdd6
Arjun Gill <arjungill6ep@gmail.com>
2024-03-27 18:31:35 +0000

    feat: KYC disclaimer, navbar icons

--- app/(trip)/[trip_id]/card-details/kyc.tsx
+++ app/(trip)/[trip_id]/card-details/kyc.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { WeavrUserCreationFlow } from '@/lib/weavr-create-user';
 import { Button, Spinner } from '@nextui-org/react';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function KYC({ tripId }: { tripId: string }) {
@@ -13,6 +14,23 @@ export default function KYC({ tripId }: { tripId: string }) {
 
   return (
     <div className="max-w-[640px] md:m-auto md:w-full">
+      <strong>Welcome to CogoPay.</strong>
+      <br />
+      This feature allows you to create virtual cards for each trip you
+      organise, and allow for easy spend management when booking travel and
+      paying for things whilst on the trip. <br /> To get started, you must
+      upgrade your account to become compliant for banking. Then you will be
+      able to create cards easily for any trip.
+      <br />
+      <strong>
+        If you do not wish to use this feature, you can return to your trip
+        details.
+      </strong>
+      <Link href={`/${tripId}`}>
+        <Button className="w-full bg-primary-500 text-white my-5">
+          Go back to trip
+        </Button>
+      </Link>
       {loading ? (
         <Spinner className="w-full" />
       ) : (
